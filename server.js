415839734bf4ccc792a4cda7f4c594559cffe4ad
Samuel3131 <samsihotang02@gmail.com>
2021-08-26 01:13:18 +0700

    ubah require('dotenv/config') pada server js menjadi require('dotenv').config() pada server.js untuk konsistensi

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-require('dotenv/config');
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 const route = require('./router/routes');
